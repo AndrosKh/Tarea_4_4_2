8bb1a66b8004c3c2dfa68552d907dfd7028837d1
GS <GS@T3INF-2>
2025-02-17 15:16:59 +0100

    Primer comentario

---
+++ src/main/java/com/mycompany/tarea_4_4_2/Tarea_4_4_2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tarea_4_4_2;
+
+/**
+ *
+ * @author GS
+ */
+public class Tarea_4_4_2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Andrii");
+
+    }
+}
